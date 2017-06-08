176d268f2aa34bfa2d7efc8ce56b3be316a4ea2d
Luis Montealegre <montealegreluis@gmail.com>
2017-06-08 11:14:11 -0500

    LoginServlet is now showing the login form

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -2,5 +2,18 @@
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 
-public class LoginServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
 }
