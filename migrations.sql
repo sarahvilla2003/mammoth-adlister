c70d1a71d4631c4628eda8489ebf005371f93526
Luis Montealegre <montealegreluis@gmail.com>
2017-06-12 11:01:54 -0500

    Create migrations file for tables users and ads.

---
+++ migrations.sql
@@ -0,0 +1,21 @@
+USE adlister_db;
+
+DROP TABLE IF EXISTS ads;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(250),
+  PASSWORD VARCHAR(250),
+  email    VARCHAR(250),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE ads (
+  id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  title       VARCHAR(300),
+  description TEXT,
+  user_id     INT UNSIGNED,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -2,5 +2,46 @@
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 
-public class MySQLAdsDao {
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+
+    private Connection connection;
+
+    public MySQLAdsDao(Config config) throws SQLException {
+        DriverManager.registerDriver(new Driver());
+        connection = DriverManager.getConnection(
+            config.getUrl(), config.getUser(), config.getPassword()
+        );
+    }
+
+    @Override
+    public List<Ad> all() {
+        Statement statement = null;
+        List<Ad> ads = new ArrayList<>();
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM ads");
+            while (resultSet.next()) {
+                ads.add(new Ad(
+                    resultSet.getLong(1),
+                    resultSet.getLong(2),
+                    resultSet.getString(3),
+                    resultSet.getString(4)
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ads;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
 }
