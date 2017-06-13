98796cb78c8b38bbe3d1dd269b0b605384d1beba
Luis Montealegre <montealegreluis@gmail.com>
2017-06-13 12:22:27 -0500

    Use the database to authenticate users.

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,17 +24,22 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        // TODO: find a record in your database that matches the submitted password
-        // TODO: make sure we find a user with that username
-        // TODO: check the submitted password against what you have in your database
-        boolean validAttempt = false;
+        User user = DaoFactory.getUsersDao().findByUsername(username);
+
+        if (user == null) {
+            request.setAttribute("error", "Either your username or password are incorrect");
+            request.getRequestDispatcher("/WEB-INF/login.jsp");
+            return;
+        }
+
+        boolean validAttempt = user.getPassword().equals(password);
 
         if (validAttempt) {
-            // TODO: store the logged in user object in the session, instead of just the username
-            request.getSession().setAttribute("user", username);
+            request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
         } else {
-            response.sendRedirect("/login");
+            request.setAttribute("error", "Either your username or password are incorrect");
+            request.getRequestDispatcher("/WEB-INF/login.jsp");
         }
     }
 }
